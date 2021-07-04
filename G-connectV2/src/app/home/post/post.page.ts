0f628b8a603be494f4c1e44b81e5cf03267598ca
Shrijal1 <61082078+Shrijal1@users.noreply.github.com>
2021-07-04 15:00:35 +0530

    pro-edited

--- G-connectV2/src/app/home/post/post.page.ts
+++ G-connectV2/src/app/home/post/post.page.ts
@@ -1,9 +1,12 @@
+import { map } from 'rxjs/operators';
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Post } from 'src/app/models/post.model';
 import { RegUser } from 'src/app/models/reg-user.model';
 import { PostService } from 'src/app/services/post.service';
 import { RegUserService } from 'src/app/services/reg-user.service';
+import { Pipe, PipeTransform } from '@angular/core';
 
 @Component({
   selector: 'app-post',
@@ -16,6 +19,7 @@ export class PostPage implements OnInit {
   cUser: RegUser;
  posts: Post[];
 teacher= false;
+date= new Date();
 
 
   constructor(private router: Router, private regS: RegUserService, private postS: PostService, private route: ActivatedRoute) {
@@ -29,15 +33,22 @@ teacher= false;
 ionViewDidEnter(){
   this.postS.loadAllPosts().subscribe( val =>{
     this.posts=val;
+    val.sort((a: Post, b: Post): number =>
+    {
+      if(a.date>b.date){
+          return;
+      }
+    });
     this.cUser=this.regS.cUser;
     console.log(this.cUser);
-  });
+  }) ;
 
 
 }
 onPost(){
-  const newPost= new Post(this.content,this.cUser.name,new Date());
+  const newPost= new Post(this.content,this.cUser.name,this.date);
   this.postS.addPost(newPost);
 }
 
 }
+// .pipe(map(arr => arr.sort((a: Post , b: Post) => a.date>b.date)))
